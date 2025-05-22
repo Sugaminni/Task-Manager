782e6a72fa4f64f24edc7bfeb90a2d2454037bb7
Alex Lu <Alexhonglu@gmail.com>
2025-05-22 01:42:04 -0400

    Created basic methods for FolderManager

--- src/TaskManager/service/FolderManager.java
+++ src/TaskManager/service/FolderManager.java
@@ -1,4 +1,63 @@
 package TaskManager.service;
 
+import TaskManager.model.Task;
+
+import java.util.*;
+
 public class FolderManager {
+
+    // Folder class to allow users to create folders and store tasks within
+
+    private Map<String, List<Task>> folders = new HashMap<>();
+    private String currentFolder = "All Tasks";
+
+    // Method to create a folder
+    public void createFolder(String folderName) {
+        folders.put(folderName, new ArrayList<>());
+    }
+
+    // Methods to check if folder exists
+    public boolean folderExists(String folderName) {
+        return folders.containsKey(folderName);
+    }
+
+    // Method to get the tasks within the folder
+    public List<Task> getTasksInFolder(String folderName) {
+        return folders.get(folderName);
+    }
+
+    // Method to add tasks to a folder
+    public void addTaskToFolder(String folderName, Task task) {
+        if (!folders.containsKey(folderName)) {
+            System.out.println("Folder does not exist: " + folderName);
+            return;
+        }
+        folders.get(folderName).add(task);
+
+    }
+
+    // Displays a list of folders
+    public Set<String> listFolders () {
+        return folders.keySet();
+    }
+
+    // Method to delete a folder
+    public void deleteFolder (String folderName) {
+        folders.remove(folderName);
+    }
+
+    // Method to set the current folder, changes to all tasks if a folder does not exist yet
+    public void setCurrentFolder (String folderName) {
+        currentFolder = folderName;
+    }
+
+    // Getter for current folder
+    public String getCurrentFolder () {
+        return currentFolder;
+    }
+
+    // Method to get tasks within the current folder
+    public List<Task> getTasksInCurrentFolder () {
+        return folders.get(currentFolder);
+    }
 }
